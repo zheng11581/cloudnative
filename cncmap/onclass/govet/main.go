1585ba148a3180a72f54ff1f71dde4826d74f5de
郑海成 <zhenghc@gih.cn>
2021-12-20 15:49:47 +0800

    go vet demo

--- cncmap/onclass/govet/main.go
+++ cncmap/onclass/govet/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
 )
 
+type customer struct {
+	name string
+	sex  string
+	age  int8
+}
+
+func (c *customer) hello() (*customer, error) {
+	fmt.Printf("Hello, %s\n", c.name)
+	err := fmt.Errorf("%s\n", "This is an error")
+	return c, err
+}
+
+func (c *customer) bye() {
+	fmt.Printf("Bye Bye, %s\n", c.name)
+}
+
 func main() {
-	resp, err := http.Get("https://www.spreadsheetdb.io/")
-	defer resp.Body.Close()
+	c := customer{
+		name: "zhenghaicheng",
+		sex:  "male",
+		age:  33,
+	}
+	c1, err := c.hello()
+	defer c1.bye()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//res, err := http.Get("https://www.spreadsheetdb.io/")
+	//defer res.Body.Close()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 }
