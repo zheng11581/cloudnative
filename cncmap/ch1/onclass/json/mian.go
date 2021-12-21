c223145891cc6de4f8d580b3780b3fea3057cf66
郑海成 <zhenghc@gih.cn>
2021-12-21 16:09:57 +0800

    1. json.Marshal(to string) and json.Unmarshal(to struct) 2. map[string]interface{} save any type

--- cncmap/ch1/onclass/json/mian.go
+++ cncmap/ch1/onclass/json/mian.go
@@ -4,23 +4,36 @@ import (
 	"cncamp/ch1/onclass/interface"
 	"encoding/json"
 	"fmt"
+	"log"
 )
 
-func Unmarshal2SHuman(objStr string) demoif.IF {
-
-	h := demoif.Human{}
-	err := json.Unmarshal([]byte(objStr), &h)
+func Unmarshal2Struct(objStr string) interface{} {
+	var obj interface{}
+	err := json.Unmarshal([]byte(objStr), &obj)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	return h
+	objMap, ok := obj.(map[string]interface{}) // map[string]interface{}和[]interface{}保存任意对象
+	if !ok {
+		log.Fatal("panic")
+	}
+	for k, v := range objMap {
+		switch value := v.(type) {
+		case interface{}:
+			return fmt.Sprintf("%s %s", k, value) // 根据value类型判断
+			//case string:
+			//	return fmt.Sprintf("%s %s", k, value)
+		}
+	}
+	return obj
 }
 
-func Marshal2String(obj *demoif.IF) string {
-	objBytes, err := json.Marshal(&obj)
+func Marshal2String(obj interface{}) string {
+	objBytes, err := json.Marshal(obj)
 	if err != nil {
 		fmt.Println(err)
 	}
+	log.Println(string(objBytes))
 	return string(objBytes)
 }
 
@@ -45,9 +58,9 @@ func main() {
 
 	for _, f := range interfaces {
 		//fmt.Println(f.GetName())
-		objString := Marshal2String(&f)
-		obj := Unmarshal2SHuman(objString)
-		fmt.Println(obj) // 只有Human被Unmarshal
+		objString := Marshal2String(f)
+		obj := Unmarshal2Struct(objString)
+		fmt.Println(obj)
 	}
 
 }
