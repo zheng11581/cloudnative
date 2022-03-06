b89b20ea256b293a5524af6aa3bfbd6fe4f5bd9d
zhenghc <zhenghc@borenhospital.com>
2022-03-07 07:51:44 +0800

    cluster-api

--- cncamp/ch9/kind/cluster-api/generate_workload_cluster.sh
+++ cncamp/ch9/kind/cluster-api/generate_workload_cluster.sh
@@ -1,5 +1,5 @@
 clusterctl generate cluster capi-quickstart --flavor development \
-  --kubernetes-version v1.23.4 \
+  --kubernetes-version v1.23.3 \
   --control-plane-machine-count=1 \
   --worker-machine-count=1 \
   > capi-quickstart.yaml
