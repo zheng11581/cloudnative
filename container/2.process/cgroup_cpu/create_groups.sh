9a1e6afbd3f103faa8ce3655689ae073897a47cb
zhenghc <zhenghc@borenhospital.com>
2022-04-17 23:13:40 +0800

    2.process/cgroup_cpu

---
+++ container/2.process/cgroup_cpu/create_groups.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /sys/fs/cgroup/cpu/group1
+mkdir -p /sys/fs/cgroup/cpu/group2
+mkdir -p /sys/fs/cgroup/cpu/group2/group3
+mkdir -p /sys/fs/cgroup/cpu/group2/group4
+
+make -C threads-cpu/
