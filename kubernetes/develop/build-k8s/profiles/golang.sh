a4f6224bf28a5f72c57c85273ee26fe232f9eef9
zheng11581 <zheng11581@sina.com>
2023-03-13 17:58:52 +0800

    prepare dev env

---
+++ kubernetes/develop/build-k8s/profiles/golang.sh
@@ -0,0 +1,5 @@
+export GOPATH=/root/go
+export GOBIN=$GOPATH/bin
+export GOPROXY="https://goproxy.cn,direct"
+export GO111MODULE="on"
+export PATH=/usr/local/go/bin:$GOBIN:$PATH
---
+++ kubernetes/develop/build-k8s/profiles/kubernetes.sh
@@ -0,0 +1,1 @@
+export PATH=/root/go/src/k8s.io/kubernetes/cluster:$PATH
